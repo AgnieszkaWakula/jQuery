c97b521c0fed851f0ccb37170038de092b6dda90
Agnieszka Wakula <agnieszka.wakula@gmail.com>
2017-06-25 20:00:58 +0200

    jQuery.10.2.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // scripts.js
 
-var span = $("span");
+var span = $('span');
 span.each(function(index, element) {
 	if(index % 2 == 0) {
 		$(element).css('color', 'red');
@@ -11,11 +11,11 @@ span.each(function(index, element) {
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-    $(element).append(button) 
+    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    $(element).append(button);
 });
 
-$("button").click(function(){
+$('button').click(function(){
 	alert($(this).attr("data-tmp"));
 });
 
